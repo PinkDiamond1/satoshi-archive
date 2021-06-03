c470c9b4d9fe7b290c16a81eae386d5bbd0afe98
Cat <cat@bitcoin.com>
2021-06-03 18:29:08 +0900

    switch cdn to prod, remove second mobile nav menu

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -37,19 +37,7 @@ function initNav() {
 
   const siteNav = document.getElementById('site-nav');
   const mainHeader = document.getElementById('main-header');
-  const menuButton = document.getElementById('menu-button');
 
-  jtd.addEvent(menuButton, 'click', function(e){
-    e.preventDefault();
-
-    if (menuButton.classList.toggle('nav-open')) {
-      siteNav.classList.add('nav-open');
-      mainHeader.classList.add('nav-open');
-    } else {
-      siteNav.classList.remove('nav-open');
-      mainHeader.classList.remove('nav-open');
-    }
-  });
 
   {%- if site.search_enabled != false and site.search.button %}
   const searchInput = document.getElementById('search-input');
